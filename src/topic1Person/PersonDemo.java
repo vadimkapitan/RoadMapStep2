0966d267df82c19a7fc8b3ea0466953f779ada0b
Capmashine <xxxcap13@gmail.com>
2025-04-15 22:07:50 +0300

    topic 1 is done

---
+++ src/topic1Person/PersonDemo.java
@@ -0,0 +1,8 @@
+package topic1Person;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person tomHardy = new Person("Tom Hardy", 38);
+        tomHardy.displayInfo();
+    }
+}
